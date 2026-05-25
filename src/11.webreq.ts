0534c4a209ab5cf6471c03b6185991b15b1f7768
Sanskar Kolte <koltesanskar.1406@gmail.com>
2026-05-25 15:48:14 +0530

    ..

---
+++ src/11.webreq.ts
@@ -0,0 +1,24 @@
+// see when kahi se data ata he ya kahi ko data hu send karte he 
+// what we do is > firstly hum uss sab data ka foarmiting ke lkye ya basically hum uss data ka structre defie kar dete he >> by using INTERFACE
+
+// user:{
+
+//     taskId:2323,
+//     userId:12,
+//     taskName:"Kaam he kuch ",
+//     category:"study "
+
+// }
+
+// suppose aise data aa rah he >>
+// kahi se fetch karnan he
+
+interface user{
+    taskId:number;
+    userId:number;
+    taskName:string;
+    category:string;
+}
+
+// like this 
+
